b889b6a0825c1af7fa8d685cc605de8d97f505ce
bazgab <gabrielbzzt@gmail.com>
2024-01-26 10:17:40 -0300

    Update room_booking.py

--- tests/room_booking.py
+++ tests/room_booking.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.common.exceptions import NoSuchElementException
 
 os.environ['PATH'] += r'home/bazgab/Desktop/web/selenium_drivers'
 driver = webdriver.Chrome()
@@ -20,17 +20,36 @@ def test_case_one():
     booking_button = driver.find_element(By.XPATH, '//button[contains(@class, "openBooking")]')
     booking_button.click()
     driver.implicitly_wait(10)
-    first_element = driver.find_element(By.XPATH, '//button[contains(@class, "rbc-button-link") and contains(text(), "07")]')
-    second_element = driver.find_element(By.XPATH, '//button[contains(@class, "rbc-button-link") and contains(text(), "10")]')
+    first_element = driver.find_element(By.XPATH, '//button[contains(@class, "rbc-button-link") and contains(text(), "02")]')
+    second_element = driver.find_element(By.XPATH, '//button[contains(@class, "rbc-button-link") and contains(text(), "04")]')
     
     action = ActionChains(driver)
     action.click_and_hold(first_element).move_to_element(second_element).release().perform()
     time.sleep(5)
     action.drag_and_drop(first_element, second_element).perform()
-   
-    room_name = driver.find_element(By.XPATH, '//input[contains(@class, "room-name")]')
+    #Finding elements
+    room_firstname = driver.find_element(By.XPATH, '//input[contains(@class, "room-firstname")]')
+    room_lastname = driver.find_element(By.XPATH, '//input[contains(@class, "room-lastname")]')
+    room_email = driver.find_element(By.XPATH, '//input[contains(@class, "room-email")]')
+    room_phone = driver.find_element(By.XPATH, '//input[contains(@class, "room-phone")]')
+    book_button = driver.find_element(By.XPATH, '//button[contains(@class, "book-room")]')
+    #Perform actions
+    room_firstname.send_keys('Sample')
+    room_lastname.send_keys('User')
+    room_email.send_keys('sampleuser@provider.com')
+    room_phone.send_keys('12345678')
+    book_button.click()
+    
+def test_case_one_validation():
+    try:
+        confirmation_modal = driver.find_element(By.XPATH, '//*[contains(text(), "Booking Successful!")]')
+        if (confirmation_modal.is_displayed):
+            print('room_booking test case 1... -> Success.')    
+    except NoSuchElementException:
+        print('room_booking test case 1... -> Fail.')
     
 get_index_page()   
 test_case_one()
-time.sleep(10)
+test_case_one_validation()
+time.sleep(20)
 driver.quit()
